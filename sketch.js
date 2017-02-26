6d5a986d261998e56965a55f876c9121db15faf3
alipgn <alice.paganini@mail.polimi.it>
2017-02-26 04:16:19 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,24 @@
-var moon;
-var bombs = []
-    bombSpeed = 6,
-    xPosCorrectionScale = 1;;
-var s;
-var t;
-var millis;
-var interval;
-var pointillismAreaHeight = 300,
+var pointillismAreaHeight = 290, //a schermo interno è il punto dove tocca l'orizzonte
     points = [],
     xPosCorrectionScale = 1,
     pointSizeRange = [5, 10];
 
-//var victims = 0;
+var t;
+var moon;
+var bombs = [],
+    frameRate = 1, //non funziona, per velocizzare per fare le scie omogenee
+    bombSpeed = 4,
+    xPosCorrectionScale = 1;
+var s = second();
+var clicks = 0;
+
 
 //var skylineHeight = windowHeight/2-40;
 //var newSHeight = windowWidth*1530/2048;
 //per cercare di non scrivere ogni volta la formula sulle stringhe di comando dello skyline ma se la applico non funziona//windowWidth*1530/2048
 
 function preload() {
-dresda=loadImage("assets/citta.png");
+dresda = loadImage("assets/citta.png");
 dresda2 = loadImage("assets/citta2.png");
 dresda3 = loadImage("assets/citta3.png");
 dresda4 = loadImage("assets/citta4.png");
@@ -30,35 +30,76 @@ moon = loadImage("assets/moon.png");
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  initWidth = windowWidth;
-}
+ initWidth = windowWidth;
+    }
 
 
 function draw() {
+
 imageMode(CENTER);
+
+    //image(dresda, windowWidth/2, windowHeight/2, windowWidth, windowHeight);
     
-  if(bombs.length < 100) {
-        image(dresda, windowWidth/2, windowHeight/2, windowWidth, windowWidth*1530/2048);
+    if(bombs.length < 100) {
+        //backgroundImage(dresda)
+        image(dresda, windowWidth/2, windowHeight/2, windowWidth, windowWidth/2);
+
     }
-    if( bombs.length > 100 ) {
+  if( bombs.length > 100 ) {
+        //backgroundImage(dresda)
         image(dresda2, windowWidth/2, windowHeight/2,windowWidth, windowWidth*1530/2048)
-    } 
+        
+    }
     if(bombs.length > 200) {
+        //backgroundImage(dresda)
         image(dresda3, windowWidth/2, windowHeight/2,windowWidth, windowWidth*1530/2048);
     }
     if(bombs.length > 300) {
+        //backgroundImage(dresda)
         image(dresda4, windowWidth/2, windowHeight/2,windowWidth, windowWidth*1530/2048);
     }
     if(bombs.length > 400) {
+        //backgroundImage(dresda)
         image(dresda5, windowWidth/2, windowHeight/2,windowWidth, windowWidth*1530/2048);
     }
-    if(bombs.length > 500) {
+     if(bombs.length > 500) {
+        //backgroundImage(dresda)
         image(dresda6, windowWidth/2, windowHeight/2,windowWidth, windowWidth*1530/2048);
-     }
+    }   
     if(bombs.length > 600) {
+        //backgroundImage(dresda)
         image(dresda7, windowWidth/2, windowHeight/2,windowWidth, windowWidth*1530/2048);
     }
- for (var i = 0; i < bombs.length; i++) {
+    
+    //bombs
+    //iterate on the array
+    
+    
+  //IL TUO CODICE PER LE BOMBE
+    
+//  for(var i=0; i< bombs.length; i++) {
+//    // Increase the single bomb y position
+//    bombs[i].y+=5;
+//    
+//    // Create a new ellipse using the x and y properties of the bomb object
+//    ellipse(bombs[i].x, bombs[i].y, 4,10);
+//    
+//  if (bombs[i].y > height- pointillismAreaHeight) { 
+//    fill(255,0,0)
+//    noStroke();
+//    ellipse(bombs[i].x, windowHeight, bombs[i].size);
+//    //bombs.splice(i,7);
+//    
+//   } else {
+//        fill(bombs[i].color),
+//        noStroke();
+//        
+//      }
+//  }
+    
+ //IL MIO CODICE PER LE BOMBE
+    
+    for (var i = 1; i < bombs.length; i++) {
         if (bombs[i].y > height - pointillismAreaHeight) {
             var bomb = bombs[i];
             ellipse(bombs[i].x, bombs[i].y,50,50);
@@ -95,8 +136,9 @@ if (!points[i]) {
                 }
             }
     
-var s = second() + 30
-image(moon, width/5 +s, height/3 -s, 70, 70);
+var s = second()
+var t = interval-int(millis()/1000);
+image(moon, width/5 + s, height/3 - s, 70, 70);
     
 var interval = 120;
 var millisecond = millis()
@@ -118,19 +160,10 @@ text("Victims",1345,80);
 textSize(25);
 text(s,1300, 80);
 
-    if(t === 0){
-    noStroke();
-    fill(0, 0, 0);
-    rect(0, 0, windowWidth, windowHeight);
-    textSize(32);
-    fill(250);
-    textAlign(CENTER);
-    text("Remember dresda", width/2, height/2);
-    t(0,1); 
-    }
-    
-/* drawPointillism();*/
+    drawPointillism();
 }
+
+
 
 function mouseReleased() {
   var newx = random()*width;
@@ -151,10 +184,10 @@ function mouseReleased() {
 
 }
 
-/*function drawPointillism() {
+function drawPointillism() {
     // draw rect
     noStroke();
-    fill(15);
+    fill(10);
     rect(0, height - pointillismAreaHeight, width, pointillismAreaHeight);
 
     // draw point
@@ -163,8 +196,9 @@ function mouseReleased() {
         fill(80);
         noStroke();
         ellipse(point.x * xPosCorrectionScale, point.y, point.size);
-    }*/
-
+    }
+}
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  xPosCorrectionScale = windowWidth / initWidth;
   }
